93dc5f0b4f019cdb8a6340f01097645e66019429
Daniel <danieltjandra@gmail.com>
2017-03-26 17:44:06 -0400

    Image enhancements.
    Show the image in a smaller size in the form. Otherwise if the original image is high res it it will eat most of the form space.
    Make it possible to get the full sized image by clicking on it. Will open a new tab or window.

---
+++ app/assets/javascripts/examinations.js
@@ -0,0 +1,8 @@
+//open the image full sized in a new tab/window
+$("#dicom_image").on("click", function()
+{
+	w = window.open();
+	fullsize = $("#dicom_image").clone();
+	fullsize.height("auto"); //make the image its original size
+	fullsize.appendTo($(w.document.body));
+});
